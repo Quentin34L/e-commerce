0f1668fbb330396f0b3abd653563915fa3f8b478
lequentin <quentin.letessier@ynov.com>
2025-03-21 16:17:36 +0100

    boom

--- BDD.sql
+++ BDD.sql
@@ -1,10 +1,9 @@
--- Création de la base de données
+
 CREATE DATABASE IF NOT EXISTS ShopOnline;
 
--- Sélectionner la base de données à utiliser
+
 USE ShopOnline;
 
--- Création de la table des utilisateurs
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL,
@@ -13,7 +12,6 @@ CREATE TABLE IF NOT EXISTS users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- Création de la table des produits
 CREATE TABLE IF NOT EXISTS products (
     id INT AUTO_INCREMENT PRIMARY KEY,
     product_name VARCHAR(255) NOT NULL,
@@ -23,7 +21,6 @@ CREATE TABLE IF NOT EXISTS products (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- Création de la table des commandes
 CREATE TABLE IF NOT EXISTS orders (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT,
@@ -32,7 +29,6 @@ CREATE TABLE IF NOT EXISTS orders (
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
--- Création de la table des éléments de commande (produits dans une commande)
 CREATE TABLE IF NOT EXISTS order_items (
     order_id INT,
     product_id INT,
@@ -42,7 +38,6 @@ CREATE TABLE IF NOT EXISTS order_items (
     FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE
 );
 
--- Création de la table des paniers
 CREATE TABLE IF NOT EXISTS carts (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
@@ -50,7 +45,6 @@ CREATE TABLE IF NOT EXISTS carts (
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
--- Création de la table des éléments du panier (produits dans le panier)
 CREATE TABLE IF NOT EXISTS cart_items (
     cart_id INT,
     product_id INT,
